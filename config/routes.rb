2403a6fe24545ec4e65897126bf17b848956e440
Taller2 <taller2@taller2-VirtualBox.(none)>
2014-05-30 03:40:59 -0500

    Work

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Demo::Application.routes.draw do
+  resources :comments
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root 'comments#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -53,4 +57,5 @@ Demo::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root :to =>'users#index'
 end
